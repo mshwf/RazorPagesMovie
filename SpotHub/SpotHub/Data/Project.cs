5be2c84fbd9dab22046eae8813e86be64cb27003
Mohamed Elshawaf <m_shawaf@outlook.com>
2018-07-13 08:18:20 +0200

    Cretae SpotHub

---
+++ SpotHub/SpotHub/Data/Project.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SpotHub.Data
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int ManagerId { get; set; }
+        [ForeignKey("ManagerId")]
+        public virtual ApplicationUser Manager { get; set; }
+        public virtual ICollection<ApplicationUser> Team { get; set; }
+    }
+}
---
+++ SpotHub/SpotHub/Data/Spot.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpotHub.Data
+{
+    public enum Priority
+    {
+        Urgent, Normal
+    }
+    public class Spot
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int UserId { get; set; }
+        public int? ProjectId { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public Priority Priority { get; set; }
+        [ForeignKey("UserId")]
+        public virtual ApplicationUser User { get; set; }
+        [ForeignKey("ProjectId")]
+        public virtual Project Project { get; set; }
+    }
+}
